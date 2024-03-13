98a34aebd8904fd487b0a7ec78f27fd8ec769d5f
Sevastian Bajenov <sevabaj@gmail.com>
2024-03-13 21:52:02 +0200

    Add Vehicle class with basic fields and methods.

--- OOP_practice/Program.cs
+++ OOP_practice/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace OOP_practice
+﻿using OOP_practice.entities;
+
+namespace OOP_practice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Vehicle v = new Vehicle("Volvo", "XC-90", 300);
+            v.Drive();
+            Console.WriteLine(v.Model);
         }
     }
 }
---
+++ OOP_practice/entities/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿namespace OOP_practice.entities
+{
+    public class Vehicle
+    {
+        private string manufacturer;
+        public string Manufacturer { get { return manufacturer; } }
+
+        private string model;
+        public string Model { get { return model; } }
+
+        private int power;
+        public int Power
+        {
+            get
+            {
+                return power;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Power cannot be negative or zero");
+                } else
+                {
+                    power = value;
+                }
+            }
+        }
+
+        public Vehicle(string manufacturer, string model, int power)
+        {
+            this.manufacturer = manufacturer;
+            this.model = model;
+            Power = power;
+        }
+
+        public virtual void Drive()
+        {
+            Console.WriteLine("The vehicle drives");
+        }
+
+        public virtual void Drive(int speed)
+        {
+            Console.WriteLine("The vehicle drives at speed " + speed);
+        }
+    }
+}
