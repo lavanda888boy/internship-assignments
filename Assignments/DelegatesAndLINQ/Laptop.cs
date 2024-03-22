f9492112ed4e609a254f9489b45b90e5c0bd4e6c
Sevastian Bajenov <sevabaj@gmail.com>
2024-03-22 10:34:49 +0200

    Add new data for testing advanced linq features.

---
+++ Assignments/DelegatesAndLINQ/Laptop.cs
@@ -0,0 +1,21 @@
+﻿namespace DelegatesAndLINQ
+{
+    internal class Laptop
+    {
+        private string _manufacturer;
+        public string Manufacturer => _manufacturer;
+
+        private string _model;
+        public string Model => _model;
+
+        private Employee _owner;
+        public Employee Owner => _owner;
+
+        public Laptop(string manufacturer, string model, Employee owner)
+        {
+            _manufacturer = manufacturer;
+            _model = model;
+            _owner = owner;
+        }
+    }
+}
--- Assignments/DelegatesAndLINQ/Program.cs
+++ Assignments/DelegatesAndLINQ/Program.cs
@@ -1,22 +1,31 @@
-﻿using System;
-
-namespace DelegatesAndLINQ
+﻿namespace DelegatesAndLINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            EmployeeCollection ec = new EmployeeCollection();
-            ec.Add(new Employee("Smith", "John", "john@mail.com", "762589", 8, false));
-            ec.Add(new Employee("Brenton", "Agatha", "agatha@mail.com", "235869", 6, true));
-            ec.Add(new Employee("Ross", "Mike", "mike@mail.com", "214153", 12, false));
-            ec.Add(new Employee("Specter", "Harvey", "harvey@mail.com", "849254", 12, false));
-            ec.Add(new Employee("Halpert", "Jim", "jim@mail.com", "389575", 4, true));
-            ec.Add(new Employee("Schrute", "Dwight", "dwight@mail.com", "959559", 10, false));
+            Employee john = new Employee("Smith", "John", "john@mail.com", "762589", 8, false);
+            Employee mike = new Employee("Ross", "Mike", "mike@mail.com", "214153", 12, false);
+            Employee agatha = new Employee("Brenton", "Agatha", "agatha@mail.com", "235869", 6, true);
+            Employee harvey = new Employee("Specter", "Harvey", "harvey@mail.com", "849254", 12, false);
+            Employee jim = new Employee("Halpert", "Jim", "jim@mail.com", "389575", 4, true);
+            Employee dwight = new Employee("Schrute", "Dwight", "dwight@mail.com", "959559", 10, false);
+
+            EmployeeCollection ec = [ john, mike, agatha, harvey, jim, dwight ];
+
+            List<Laptop> devices =
+            [
+                new Laptop("Dell", "Latitude", john),
+                new Laptop("Dell", "XPS", mike),
+                new Laptop("Dell", "Vostro", agatha),
+                new Laptop("Lenovo", "Thinkpad", harvey),
+                new Laptop("Lenovo", "Thinkbook", jim),
+                new Laptop("Macbook", "Pro", dwight)
+            ];
 
             /*var e = ec.Find(employee => !employee.IsOnVacation);*/
 
-            /*Func<Employee, bool> SearchDelegate = delegate (Employee employee)
+            Func<Employee, bool> SearchDelegate = delegate (Employee employee)
             {
                 if (employee.WorkingDayLength < 10)
                 {
@@ -27,23 +36,28 @@ namespace DelegatesAndLINQ
                     return false;
                 }
             };
-            var e = ec.Find(SearchDelegate);*/
+            var e = ec.Find(SearchDelegate);
 
             /*Func<Employee, bool> SearchDelegate = (employee) => employee.Phone.StartsWith("2");
             var e = ec.Find(SearchDelegate);*/
 
             /*var e = ec.Employees.Where(employee => !employee.IsOnVacation)
-                                .OrderByDescending(employee => employee.WorkingDayLength)
-                                .Select(employee => employee);*/
+                                .OrderByDescending(employee => employee.WorkingDayLength);*/
 
-            var e = ec.Employees.Where(employee => employee.WorkingDayLength < 10)
+            /*var e = ec.Employees.Where(employee => employee.WorkingDayLength < 10)
                                 .OrderBy(employee => employee.Surname)
                                 .Skip(1)
-                                .Select(employee => employee);
+                                .Select(e => new
+                                {
+                                    Surname = e.Surname,
+                                    Name = e.Name,
+                                    WorkingDayLength = e.WorkingDayLength
+                                });*/
 
-            foreach (var employee in e)
+            foreach (var em in e)
             {
-                employee.PrintEmployeeInfo();
+                //Console.WriteLine(em);
+                em.PrintEmployeeInfo();
             }
         }
     }
