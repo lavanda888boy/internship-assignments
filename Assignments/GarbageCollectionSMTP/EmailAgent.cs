a0b0fa13473cd7b7a789d639434e59838030c1cb
Sevastian Bajenov <sevabaj@gmail.com>
2024-03-24 23:17:53 +0200

    Implement smtp server notification.

---
+++ Assignments/GarbageCollectionSMTP/EmailAgent.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Mail;
+using System.Net;
+using System.Security;
+
+namespace GarbageCollectionSMTP
+{
+    internal class EmailAgent
+    {
+        private readonly string _hostEmailAddress;
+        private readonly string _smtpServerAddress;
+        private readonly string _smtpServerPassword;
+
+        public EmailAgent(string hostEmail, string serverAddress, string serverPassword)
+        {
+            _hostEmailAddress = hostEmail;
+            _smtpServerAddress = serverAddress;
+            _smtpServerPassword = serverPassword;
+        }
+
+        public void SendEmail(string recipientEmail)
+        {
+            if (recipientEmail is null)
+            {
+                throw new ArgumentNullException("Recipient email cannot be null");
+            }
+
+            string subject = "Thank you for subscribing!";
+            string body = "Thank you for subscribing to our newsletter.";
+
+            using (MailMessage message = new MailMessage(_hostEmailAddress, recipientEmail, subject, body))
+            {
+                using SmtpClient smtpClient = new SmtpClient(_smtpServerAddress)
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(_hostEmailAddress, _smtpServerPassword),
+                    EnableSsl = true,
+                };
+                try
+                {
+                    smtpClient.Send(message);
+                    Console.WriteLine("Email sent successfully.");
+                }
+                catch (SmtpException ex)
+                {
+                    Console.WriteLine($"Failed to send email. Error: {ex.Message}");
+                }
+                catch (SecurityException ex)
+                {
+                    Console.WriteLine($"Email operation requires security permissions. Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ Assignments/GarbageCollectionSMTP/Program.cs
@@ -0,0 +1,25 @@
+﻿using DotNetEnv;
+
+namespace GarbageCollectionSMTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Env.Load();
+
+            Console.Write("Enter your email address: ");
+            string? emailAddress = Console.ReadLine();
+
+            EmailAgent ea = new EmailAgent(Env.GetString("HOST_EMAIL"), Env.GetString("SMTP_SERVER"), Env.GetString("APP_PASSWORD"));
+            try
+            {
+                ea.SendEmail(emailAddress);
+            }
+            catch (ArgumentNullException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
