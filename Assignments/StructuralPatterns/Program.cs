f1162ef2cbee69f4abf1959f9f5d7cac780f4139
Sevastian Bajenov <sevabaj@gmail.com>
2024-03-31 22:36:53 +0300

    Add interactive menu for the text editor.

--- Assignments/StructuralPatterns/Program.cs
+++ Assignments/StructuralPatterns/Program.cs
@@ -7,11 +7,72 @@ namespace StructuralPatterns
     {
         static void Main(string[] args)
         {
-            ITextComponent text = new TextComponent("Some text");
-            ITextEditor textEditor = new TextEditor(text);
-            Console.WriteLine(textEditor.MakeColored("red").GetText());
-            Console.WriteLine(textEditor.MakeBold().GetText());
-            Console.WriteLine(textEditor.Undo().GetText());
+            TextEditor editor = new TextEditor(new TextComponent(""));
+
+            while(true)
+            {
+                Console.WriteLine("Welcome to the text editor! Please enter the text to edit:");
+                var text = Console.ReadLine();
+                Console.WriteLine();
+
+                if (text != "")
+                {
+                    ProcessCommands(text, editor);
+                }
+            }
+        }
+
+        static void ProcessCommands(string text, ITextEditor textEditor)
+        {
+            ((TextEditor) textEditor).Text = new TextComponent(text);
+
+            while (true)
+            {
+                Console.WriteLine("Enter the command to edit the text:\n");
+                Console.WriteLine("b - make it bold");
+                Console.WriteLine("i - make it italic");
+                Console.WriteLine("u - make it underlined");
+                Console.WriteLine("c - apply color to it");
+                Console.WriteLine("z - undo last operation");
+                Console.WriteLine("s - enter new text\n");
+
+                var option = Console.ReadLine();
+                Console.WriteLine();
+                
+                switch (option)
+                {
+                    case "b":
+                        Console.WriteLine(textEditor.MakeBold().GetText());
+                        break;
+                    case "i":
+                        Console.WriteLine(textEditor.MakeItalic().GetText());
+                        break;
+                    case "u":
+                        Console.WriteLine(textEditor.Underline().GetText());
+                        break;
+                    case "c":
+                        Console.WriteLine(textEditor.MakeColored(ProcessColorAddition()).GetText());
+                        break;
+                    case "z":
+                        Console.WriteLine(textEditor.Undo().GetText());
+                        break;
+                    case "s":
+                        return;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static string ProcessColorAddition()
+        {
+            Console.WriteLine("Enter the color to apply:");
+            var color = Console.ReadLine();
+            Console.WriteLine();
+
+            return color;
         }
     }
 }
