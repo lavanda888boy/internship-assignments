e6aeabdbccbae13a123b81f1558c542f25a784ea
Sevastian Bajenov <sevabaj@gmail.com>
2024-05-28 18:46:51 +0300

    Implement simple login state management.

--- HospitalManagementSystem/Hospital.App/src/components/shared/Statusbar.tsx
+++ HospitalManagementSystem/Hospital.App/src/components/shared/Statusbar.tsx
@@ -1,10 +1,25 @@
 import "./Statusbar.css";
 import person from "../../assets/person.jpg";
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { PageContext } from "../../context/PageContext";
 
 function Statusbar() {
   const pageContextProps = useContext(PageContext);
+
+  const [loginButton, setLoginButton] = useState(false);
+  const [loginForm, setLoginForm] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userSurname, setUserSurname] = useState("");
+
+  const handleLoginButtonClick = () => {
+    setLoginForm(!loginForm);
+  };
+
+  const handleLoginFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoginButton(true);
+    setLoginForm(false);
+  };
 
   return (
     <>
@@ -18,8 +33,37 @@ function Statusbar() {
             placeholder="Search smth here"
           />
         </form>
-        <p className="header-userinfo">Name Surname</p>
+        {loginButton ? (
+          <p className="header-userinfo">
+            {userName} {userSurname}
+          </p>
+        ) : (
+          <button className="header-login" onClick={handleLoginButtonClick}>
+            Login
+          </button>
+        )}
         <img src={person} className="header-userimage" />
+        {loginForm && (
+          <div className="login-form-wrapper">
+            <form className="login-form" onSubmit={handleLoginFormSubmit}>
+              <input
+                type="text"
+                value={userName}
+                placeholder="Name"
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <input
+                type="text"
+                value={userSurname}
+                onChange={(e) => setUserSurname(e.target.value)}
+                placeholder="Surname"
+              />
+              <button type="submit" className="login-form-submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
       </header>
     </>
   );
