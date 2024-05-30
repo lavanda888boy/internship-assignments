5ae1433f0e32c14313c3585c7d8680f2c24ff5b5
Sevastian Bajenov <sevabaj@gmail.com>
2024-05-30 17:30:52 +0300

    Initialize new theme file.

--- HospitalManagementSystem/Hospital.App/src/App.tsx
+++ HospitalManagementSystem/Hospital.App/src/App.tsx
@@ -1,17 +1,20 @@
-import "./reset.css";
-import "./App.css";
 import Statusbar from "./components/shared/Statusbar";
 import Doctors from "./pages/Doctors";
 import Patients from "./pages/Patients";
 import Records from "./pages/Records";
 import { PageContextProvider } from "./context/PageContext";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
+import "./reset.css";
 
 function App() {
   return (
-    <PageContextProvider>
-      <Statusbar />
-      <Patients />
-    </PageContextProvider>
+    <ThemeProvider theme={theme}>
+      <PageContextProvider>
+        <Statusbar />
+        <Records />
+      </PageContextProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ HospitalManagementSystem/Hospital.App/src/theme.tsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9381ff",
+      light: "#B8B8FF",
+    },
+    secondary: {
+      main: "#FFD8BE",
+      light: "#FFEEDD",
+    },
+  },
+});
+
+export default theme;
